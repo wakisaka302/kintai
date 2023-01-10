67654a5c350a481ba60be7fcc8187e50dddd8ced
3030743 <3030743@30307-43>
2023-01-10 14:24:12 +0900

    frameのサイズを変更

--- src/kintai/frame.java
+++ src/kintai/frame.java
@@ -39,7 +39,7 @@ public class frame extends JFrame {
 	 */
 	public frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 1000);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
