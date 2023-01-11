666ed4a783a466cc27efe362ff1419d39765d59a
3030745 <3030745@30307-45>
2023-01-11 10:17:55 +0900

    更新

--- src/kintai/EmployeeRegistrationDownPanel.java
+++ src/kintai/EmployeeRegistrationDownPanel.java
@@ -2,7 +2,6 @@ package kintai;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
@@ -43,26 +42,32 @@ public class EmployeeRegistrationDownPanel extends JPanel {
 				}else if(rdbtnNewRadioButton_1.isSelected()) {
 					seibetu = "女";
 				}
-				
+
 				String name=textField_3.getText();
 				DbOperation DB = new DbOperation();
-				ArrayList<EmployeeData>list=DB. dbGet();
-				int max=0;
-				for(int i=0;i<list.size();i++) {
-					if(max<list.get(i).getBangou()) {
-						max=list.get(i).getBangou();
-					}
-				}if(max!=0) {
-				
+				//ArrayList<EmployeeData>list=DB. dbGet();
+				int max=DB.IdMaxGet();
+
+				//				for(int i=0;i<list.size();i++) {
+				//					if(max<list.get(i).getBangou()) {
+				//						max=list.get(i).getBangou();
+				//					}
+				if(max!=0) {
+					max = max + 1;
 					//DBに接続して社員番号の最大値を取得する
-					
+
 					//社員番号が存在するとき
-					DB.dbInsert(kihonkyu, seibetu, name);
-					}else {
+//					DB.dbInsert(max, kihonkyu, seibetu, name);
+
+				}else {
+					max = 23013021;
 					//社員番号が存在しないとき
-					DB.dbInsert(23013021,kihonkyu, seibetu, name);
-					}
-					
+//					DB.dbInsert(23013021,kihonkyu, seibetu, name);
+
+				}
+				
+				DB.dbInsert(max, kihonkyu, seibetu, name);
+				EmployeeRegistrationUpPanel.setObjectRowData(max, kihonkyu, seibetu, name);
 			}
 		});
 
@@ -77,61 +82,61 @@ public class EmployeeRegistrationDownPanel extends JPanel {
 		ButtonGroup bgroup = new ButtonGroup();
 		bgroup.add(rdbtnNewRadioButton);
 		bgroup.add(rdbtnNewRadioButton_1);
-		
+
 		JButton btnNewButton_1 = new JButton("削除");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String name=textField_3.getText();
-//				DbOperation DB = new DbOperation();
-//				DB.dbDelete(name);
+				//				DbOperation DB = new DbOperation();
+				//				DB.dbDelete(name);
 			}
 		});
 
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+				groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(21)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(33)
-							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(13))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
-							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnNewRadioButton)))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(rdbtnNewRadioButton_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(92, Short.MAX_VALUE))
-		);
+						.addGap(21)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addGap(33)
+										.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addGap(13))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+										.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(rdbtnNewRadioButton)))
+						.addGap(18)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(rdbtnNewRadioButton_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE))
+						.addContainerGap(92, Short.MAX_VALUE))
+				);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+				groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_2)
-						.addComponent(lblNewLabel_3)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(rdbtnNewRadioButton_1)
-						.addComponent(rdbtnNewRadioButton))
-					.addPreferredGap(ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_1)
-						.addComponent(btnNewButton))
-					.addContainerGap(56, Short.MAX_VALUE))
-		);
+						.addContainerGap()
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel_2)
+								.addComponent(lblNewLabel_3)
+								.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(rdbtnNewRadioButton_1)
+								.addComponent(rdbtnNewRadioButton))
+						.addPreferredGap(ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel_1)
+								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_1)
+								.addComponent(btnNewButton))
+						.addContainerGap(56, Short.MAX_VALUE))
+				);
 		setLayout(groupLayout);
 
 	}
--- src/kintai/EmployeeRegistrationUpPanel.java
+++ src/kintai/EmployeeRegistrationUpPanel.java
@@ -16,53 +16,66 @@ public class EmployeeRegistrationUpPanel extends JPanel {
 	ArrayList<EmployeeData>list;
 	DbOperation DB=new DbOperation();
 	private JTable table;
+	static DefaultTableModel tablemodel;
 	String [] columns= {"社員ID","名前","性別","基本給"};
 	/**
 	 * Create the panel.
 	 */
-	
-		
-		/**
-		 * Create the panel.
-		 */
-		public EmployeeRegistrationUpPanel() {
-			
-			//データベースから取得したリストを受け取る
-			list =DB.dbGet();
-			//defulttablemodelにリストを詰める
-			DefaultTableModel tablemodel=new DefaultTableModel(ConyertoObject(),columns);
-			table =new JTable(tablemodel);
-			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			//テーブルの列サイズ設定
-			DefaultTableColumnModel columnModel=(DefaultTableColumnModel)table.getColumnModel();
-			TableColumn column =null;
-			for(int i=0;i<columnModel.getColumnCount();i++) {
-				column =columnModel.getColumn(i);
-				column.setPreferredWidth(50);
-			}
-			//スクロールパネルにテーブルを追加
-			JScrollPane sp=new JScrollPane(table);
-			sp.setPreferredSize(new Dimension(250,100));
-			//upperpanelにスクロールパネルを追加する
-			add(sp);
+
+
+	/**
+	 * Create the panel.
+	 */
+	public EmployeeRegistrationUpPanel() {
+
+		//データベースから取得したリストを受け取る
+		list =DB.dbGet();
+		//defulttablemodelにリストを詰める
+		tablemodel=new DefaultTableModel(ConyertoObject(),columns);
+		table =new JTable(tablemodel);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		//テーブルの列サイズ設定
+		DefaultTableColumnModel columnModel=(DefaultTableColumnModel)table.getColumnModel();
+		TableColumn column =null;
+		for(int i=0;i<columnModel.getColumnCount();i++) {
+			column =columnModel.getColumn(i);
+			column.setPreferredWidth(100);
 		}
-		//ArrayList→Objectに変換する
-			private Object[][] ConyertoObject(){
-		    Object[][] ob =new Object[list.size()][columns.length];
-			
-			//リストでループ処理
-			for(int i=0;i<list.size();i++){
-				ob[i][0]=list.get(i).getBangou();
-				ob[i][1]=list.get(i).getName();
-				ob[i][2]=list.get(i).getSeibetu();
-				ob[i][3]=list.get(i).getKihon();
-				
-			}
-			//System.out.println("a");
-			return ob;
+		//スクロールパネルにテーブルを追加
+		JScrollPane sp=new JScrollPane(table);
+		sp.setPreferredSize(new Dimension(500,100));
+		//upperpanelにスクロールパネルを追加する
+		add(sp);
+	}
+	//ArrayList→Objectに変換する
+	private Object[][] ConyertoObject(){
+		Object[][] ob =new Object[list.size()][columns.length];
 
-		
+		//リストでループ処理
+		for(int i=0;i<list.size();i++){
+			ob[i][0]=list.get(i).getBangou();
+			ob[i][1]=list.get(i).getName();
+			ob[i][2]=list.get(i).getSeibetu();
+			ob[i][3]=list.get(i).getKihon();
 
+		}
+		//System.out.println("a");
+		return ob;
+
+	}
+	
+	public static void setObjectRowData(int max, int kihonkyu, String seibetu, String name) {
+		//Object[][] ob =new Object[1][4];
+		Object[] obb = new Object[4];
+//		ob[0][0]= max;
+//		ob[0][1]= kihonkyu;
+//		ob[0][2]= seibetu;
+//		ob[0][3]= name;
+		obb[0] = max;
+		obb[1] = name;
+		obb[2] = seibetu;
+		obb[3] = kihonkyu;
+		tablemodel.addRow(obb);
 	}
 
 }
