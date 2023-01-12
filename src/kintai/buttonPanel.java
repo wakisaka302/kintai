1236721acf0677267e10f060d0b3a9fb86775709
3030747 <3030747@30307-47>
2023-01-12 17:25:26 +0900

    ボタンデザイン変更テスト

--- src/kintai/buttonPanel.java
+++ src/kintai/buttonPanel.java
@@ -1,12 +1,16 @@
 package kintai;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -27,7 +31,10 @@ public class buttonPanel extends JPanel implements ActionListener{
 	public buttonPanel() {
 		
 		//社員情報登録
-		btnNewButton = new JButton("A");
+		btnNewButton = new JButton("社員情報登録");
+		btnNewButton.setFont(new Font("MS UI Gothic", Font.BOLD, 13));
+		btnNewButton.setForeground(SystemColor.window);
+		btnNewButton.setBackground(new Color(0, 51, 102));
 		btnNewButton.setSize(50, 50);
 		btnNewButton.setEnabled(false);	
 		btnNewButton.addActionListener(this);
@@ -37,7 +44,9 @@ public class buttonPanel extends JPanel implements ActionListener{
 		
 		
 		//勤務表
-		btnNewButton_1 = new JButton("B");
+		btnNewButton_1 = new JButton(" 勤務表表示 ");
+		btnNewButton_1.setFont(new Font("MS UI Gothic", Font.BOLD, 14));
+		btnNewButton_1.setBackground(new Color(0, 51, 102));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -47,18 +56,22 @@ public class buttonPanel extends JPanel implements ActionListener{
 		btnNewButton_1.setActionCommand("workSchedule");
 		
 		
-		
 		//CSVファイルチューザー
-		btnNewButton_2 = new JButton("C");
+		btnNewButton_2 = new JButton(" CSV登録 ");
+		btnNewButton_2.setFont(new Font("MS UI Gothic", Font.BOLD, 14));
+		btnNewButton_2.setBackground(new Color(0, 51, 102));
 		btnNewButton_2.setSize(50, 50);
 		btnNewButton_2.setEnabled(false);
 		btnNewButton_2.addActionListener(this);		
 		btnNewButton_2.setActionCommand("FileChooser");
 
-		btnNewButton_3 = new JButton("D");
+		btnNewButton_3 = new JButton("給与明細表示");
+		btnNewButton_3.setFont(new Font("MS UI Gothic", Font.BOLD, 14));
+		btnNewButton_3.setBackground(new Color(0, 51, 102));
 		btnNewButton_3.setEnabled(false);
 
 		JButton btnNewButton_4 = new JButton("閉じる");
+		btnNewButton_4.setIcon(new ImageIcon("C:\\Users\\3030747\\git\\kintai\\bin\\image\\×ボタン.png"));
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//「閉じる」でwindowを閉じる
@@ -76,13 +89,14 @@ public class buttonPanel extends JPanel implements ActionListener{
 				.addGroup(groupLayout.createSequentialGroup()
 					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+					.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+					.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE)
 					.addGap(6)
-					.addComponent(btnNewButton_3, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+					.addComponent(btnNewButton_3, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnNewButton_4, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+					.addComponent(btnNewButton_4, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addGap(5))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
