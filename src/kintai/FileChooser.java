c4f37c622d95e17fca19cc578935f7fae04036ff
3030743 <3030743@30307-43>
2023-01-16 13:51:20 +0900

    ファイル名を選別できるように(DBに新しいテーブルを作成する必要あり)

--- src/kintai/FileChooser.java
+++ src/kintai/FileChooser.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -18,7 +19,9 @@ public class FileChooser extends JPanel implements ActionListener{
 	private String ym;
 	private String filepath;
 	private String result = "";
-	
+	DbOperation db = new DbOperation();
+	ArrayList<AData> list;
+
 	FileChooser(){
 		setBackground(new Color(0, 64, 128));
 
@@ -35,90 +38,99 @@ public class FileChooser extends JPanel implements ActionListener{
 		btnFileSelect.addActionListener(this);
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+				groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(btnFileSelect, GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(46)
-							.addComponent(labelPanel, GroupLayout.PREFERRED_SIZE, 259, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
-		);
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(btnFileSelect, GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addGap(46)
+										.addComponent(labelPanel, GroupLayout.PREFERRED_SIZE, 259, GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap())
+				);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+				groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(18)
-					.addComponent(labelPanel, GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
-					.addGap(18)
-					.addComponent(btnFileSelect, GroupLayout.PREFERRED_SIZE, 212, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
-		);
+						.addGap(18)
+						.addComponent(labelPanel, GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
+						.addGap(18)
+						.addComponent(btnFileSelect, GroupLayout.PREFERRED_SIZE, 212, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap())
+				);
 		setLayout(groupLayout);
 	}
 
+
+
 	public void actionPerformed(ActionEvent e){
 		JFileChooser filechooser = new JFileChooser();
-
 		int selected = filechooser.showOpenDialog(this);
 		if (selected == JFileChooser.APPROVE_OPTION){
-			
 			File file = filechooser.getSelectedFile();
-			
-			
-			int fcl = file.getName().length();
-			String extension = file.getName().substring(fcl-3);
-			System.out.println(extension);
-			if( extension.equals("csv")) {
-				label.setText("ファイルを読み込みました");
-				System.out.println("ファイルを読み込みました");
-				label.setText(file.getName());
-				
-				System.out.println(file);
-				System.out.println(file.getName());
-				
-				String y = file.getName().substring(0,4);
-				String m = file.getName().substring(4,6);
-				String ym = y + "-" + m;
-				
-				System.out.println(ym);
-				
-				
-				filepath = file.toString();
-				System.out.println(filepath);
-				
+			int fcl = file.getName().length();   //ファイル名の長さを表す
+			String extension = file.getName().substring(fcl-3);   //拡張子を表す
+			if( extension.equals("csv")) {     //拡張子でファイルを判別
+				list = db.dbGetEmployeeData();
+				System.out.println(file.getName().substring(6,fcl-4));
+				int employeeNameCount = 0;    //
+				for(int i = 0; i < list.size(); i++) {  //employee_dataから名前を取ってくる
+					System.out.println("list内の社員名："+list.get(i).getName());
+					System.out.println("csvファイル名："+file.getName().substring(6,fcl-4));
+					if(list.get(i).getName().equals(file.getName().substring(6,fcl-4))) {  //とってきた名前をファイル名と比べる
+						System.out.println("true");
+						employeeNameCount++;
+						int count = 0;
+						for(String s : db.fileNameGet()) {  //DBからファイル名をとってくる
+							if(s.equals(file.getName())) {
+								System.out.println("そのファイルはすでに読み込まれています");
+								count++;
+							} 
+						}
 
-				CSV csv = new CSV(filepath, ym);
-				try {
-					csv.csvAttendance_dataReader();
-				} catch (Exception e1) {
-					// TODO 自動生成された catch ブロック
-					e1.printStackTrace();
+						if(count==0) {  
+							try {
+								db.file_nameInsert(file.getName());  //ファイル名をDBに登録
+							} catch (Exception e2) {
+								// TODO 自動生成された catch ブロック
+								e2.printStackTrace();
+							}
+							System.out.println("-----------");
+							label.setText("ファイルを読み込みました");
+							System.out.println("ファイルを読み込みました");
+							label.setText(file.getName());
+							String y = file.getName().substring(0,4);
+							String m = file.getName().substring(4,6);
+							String ym = y + "-" + m;
+							System.out.println(ym);
+
+							filepath = file.toString();
+							System.out.println(filepath);
+
+							CSV csv = new CSV(filepath, ym);
+							try {
+								csv.csvAttendance_dataReader();
+							} catch (Exception e1) {
+								// TODO 自動生成された catch ブロック
+								e1.printStackTrace();
+							}
+						}
+					} 
+				} 
+				if( employeeNameCount == 0) {  //DB内の名前とファイル名内の名前が一致しない場合
+					System.out.println("そのような社員は登録されていません");
 				}
-			
-				
-			} else {
+			}else { //拡張子が「csv」ではない場合
 				System.out.println("読み込めないファイルでした");
 				label.setForeground(Color.WHITE);
 				label.setText("拡張子が「.csv」のファイルを選んでください");
-				
-				
 			}
-
-			//////////////////////////////////////////////////////////////////////////////////////      
-			
-			
-			
-			//////////////////////////////////////////////////////////////////////////////////////
-	
-			
 		}
-		
-		
-		
-		
 	}
+
+
+
+
 	public String getYm() {
 		return ym;
 	}
