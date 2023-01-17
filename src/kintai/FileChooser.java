91fc714862c0e8d4853f03b737f97928ea05896d
3030743 <3030743@30307-43>
2023-01-17 14:16:50 +0900

    CSVファイル内のidとemployee_dataテーブル内のidを比べれるように更新(改善の余地あり)

--- src/kintai/FileChooser.java
+++ src/kintai/FileChooser.java
@@ -62,7 +62,8 @@ public class FileChooser extends JPanel implements ActionListener{
 	}
 
 	
-
+	
+	
 	public void actionPerformed(ActionEvent e){
 		JFileChooser filechooser = new JFileChooser();
 		int selected = filechooser.showOpenDialog(this);
@@ -71,62 +72,71 @@ public class FileChooser extends JPanel implements ActionListener{
 			int fcl = file.getName().length();   //ファイル名の長さを表す
 			String extension = file.getName().substring(fcl-3);   //拡張子を表す
 			if( extension.equals("csv")) {     //拡張子でファイルを判別
-				list = db.dbGetEmployeeData();
-				System.out.println(file.getName().substring(6,fcl-4));
-				int employeeNameCount = 0;    //
-				for(int i = 0; i < list.size(); i++) {  //employee_dataから名前を取ってくる
-					System.out.println("list内の社員名："+list.get(i).getName());
-					System.out.println("csvファイル名："+file.getName().substring(6,fcl-4));
-					if(list.get(i).getName().equals(file.getName().substring(6,fcl-4))) {  //とってきた名前をファイル名と比べる
-						System.out.println("true");
-						employeeNameCount++;
-						int count = 0;
-						for(String s : db.fileNameGet()) {  //DBからファイル名をとってくる
-							if(s.equals(file.getName())) {
-								System.out.println("そのファイルはすでに読み込まれています");
-								count++;
-							} 
-						}
-
-						if(count==0) {  
-							try {
-								db.file_nameInsert(file.getName());  //ファイル名をDBに登録
-							} catch (Exception e2) {
-								// TODO 自動生成された catch ブロック
-								e2.printStackTrace();
-							}
-							System.out.println("-----------");
-							label.setText("ファイルを読み込みました");
-							System.out.println("ファイルを読み込みました");
-							label.setText(file.getName());
-							String y = file.getName().substring(0,4);
-							String m = file.getName().substring(4,6);
-							String ym = y + "-" + m;
-							System.out.println(ym);
-
-							filepath = file.toString();
-							System.out.println(filepath);
-
-							CSV csv = new CSV(filepath, ym);
-							try {
-								csv.csvAttendance_dataReader();
-							} catch (Exception e1) {
-								// TODO 自動生成された catch ブロック
-								e1.printStackTrace();
-							}
-						}
-					} 
-				} 
-				if( employeeNameCount == 0) {  //DB内の名前とファイル名内の名前が一致しない場合
-					System.out.println("そのような社員は登録されていません");
+				CSV csv1 = new CSV(file.toString());
+				String id = "";
+				try {
+					id = csv1.csvAttendance_data_id_only_Reader();
+				} catch (Exception e3) {
+					e3.printStackTrace();
 				}
-			}else { //拡張子が「csv」ではない場合
+				if(db.IsId(id)) {//ファイル内idとemployee_data.employe_numberが一致
+					System.out.println("IsId : " + true);
+					list = db.dbGetEmployeeData();
+					int employeeNameCount = 0;    //
+					for(int i = 0; i < list.size(); i++) {  //employee_dataから名前を取ってくる
+						if(list.get(i).getName().equals(file.getName().substring(6,fcl-4))) {  //とってきた名前をファイル名と比べる
+							employeeNameCount++;
+							int count = 0;
+							for(String s : db.fileNameGet()) {  //DBからファイル名をとってくる
+								if(s.equals(file.getName())) {
+									System.out.println("そのファイルはすでに読み込まれています");
+									count++;
+								} 
+							}
+
+							if(count==0) {  
+								try {
+									db.file_nameInsert(file.getName());  //ファイル名をDBに登録
+								} catch (Exception e2) {
+									// TODO 自動生成された catch ブロック
+									e2.printStackTrace();
+								}
+								label.setText("ファイルを読み込みました");
+								System.out.println("ファイルを読み込みました");
+								label.setText(file.getName());
+								String y = file.getName().substring(0,4);
+								String m = file.getName().substring(4,6);
+								String ym = y + "-" + m;
+								filepath = file.toString();
+								CSV csv = new CSV(filepath, ym);
+								try {
+									csv.csvAttendance_dataReader();
+								} catch (Exception e1) {
+									// TODO 自動生成された catch ブロック
+									e1.printStackTrace();
+								}
+							}
+						} 
+					} 
+					if( employeeNameCount == 0) {  //DB内の名前とファイル名内の名前が一致しない場合
+						System.out.println("そのような社員は登録されていません");
+					} 
+				}else {//ファイル内idとemployee_data.employe_numberが不一致
+					System.out.println("idが一致しません");
+				}
+			} else { //拡張子が「csv」ではない場合
 				System.out.println("読み込めないファイルでした");
 				label.setForeground(Color.WHITE);
 				label.setText("拡張子が「.csv」のファイルを選んでください");
 			}
-		}
+
+
+		} 
 	}
+
+	
+	
+	
 
 
 
